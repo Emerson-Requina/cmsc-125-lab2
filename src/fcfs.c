a19ae5808b2110cebd118c0aa45426b047065745
Joshua Ticot <joshuaticot@gmail.com>
2026-03-12 06:44:26 +0800

    feat: add log_execution in fcfs.c

--- src/fcfs.c
+++ src/fcfs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../include/scheduler.h"
+#include "../include/gantt.h" // 1. Added this include
 
 void run_fcfs(Process *processes, int count) {
     int current_time = 0;
@@ -10,10 +11,13 @@ void run_fcfs(Process *processes, int count) {
             current_time = processes[i].arrival_time;
         }
 
-        // Record Response Time (first time it hits the CPU)
+        // Record Response Time
         processes[i].start_time = current_time;
         
-        // Execute to completion (Non-preemptive)
+        // 2. Log the execution BEFORE updating current_time
+        log_execution(processes[i].pid, current_time, current_time + processes[i].burst_time);
+
+        // Execute to completion
         current_time += processes[i].burst_time;
         processes[i].finish_time = current_time;
         
